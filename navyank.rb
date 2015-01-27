77487048d0a9761e23c1a72283672feda37007dc
Mayank Shah <mayankshah310795@gmail.com>
2015-01-28 00:08:57 +0530

    Create navyank.rb

---
+++ navyank.rb
@@ -0,0 +1,9 @@
+print "Command in Ruby"
+class BlgEntry {
+  @time = Time.now
+  @fulltext = "Mayank Shah"
+  @mood = :confused
+}
+entry1 = BlogEntry.new()
+entry2 = BlogEntry.new()
+blog = [entry1, entry2]
